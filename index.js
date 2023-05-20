c72156bbc02dfcb7c286baca47e0399deedf3c34
LydRod206 <119384486+LydRod206@users.noreply.github.com>
2023-05-19 21:01:30 -0700

    structure, index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userRoutes = require('./api/users');
+const thoughtRoutes = require('./api/thoughts');
+const reactionRoutes = require('./api/reactions');
+const friendRoutes = require('./api/friends');
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+app.use('/api/reactions', reactionRoutes);
+app.use('/api/friends', friendRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
